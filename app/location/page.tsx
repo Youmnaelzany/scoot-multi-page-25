967521595957ff3c6479759d4ef618fc34d83f57
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-11-21 01:17:22 +0200

    feat: add city request section to location page
    - Create ListedCity component with call-to-action
    - Display message prompting users to request new cities
    - Add Message Us button with hover animation
    - Integrate ListedCity into location page

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -1,3 +1,4 @@
+import ListedCity from "@/components/ListedCity";
 import PageTitles from "@/components/PageTitles";
 
 export default function LocationPage() {
@@ -6,6 +7,7 @@ export default function LocationPage() {
       <PageTitles className="bg-[url('/images/careers-location-hero-mobile.jpg')] bg-cover bg-no-repeat md:bg-[url('/images/careers-locations-hero-tablet.jpg')] lg:bg-[url('/images/careers-locations-hero-desktop.jpg')]">
         Location
       </PageTitles>
+      <ListedCity />
     </main>
   );
 }
